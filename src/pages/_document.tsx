23802803366bd80c78660bd90d44f81b88adffae
Prosenjit Chowdhury <prosenjit@tryalpine.com>
2023-02-15 00:04:14 +0600

    updated icons & manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,10 @@ class MyDocument extends Document {
           <meta name="robots" content="noindex, follow" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="English" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/icon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />

2e40fbd4c0f44ea6e84c1787605d8cb374a04a64
Prosenjit Chowdhury <prosenjit@tryalpine.com>
2023-04-15 02:52:11 +0600

    updated lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,10 +4,10 @@ module.exports = {
 
   // This will lint and format TypeScript and JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn format:fix ${filenames.join(" ")}`,
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
-  "**/*.(md|json|scss|css)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+  "**/*.(md|json|scss|css)": (filenames) => `yarn format:fix ${filenames.join(" ")}`,
 };
